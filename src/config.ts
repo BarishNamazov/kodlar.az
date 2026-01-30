88189b0a44b1e3746dce9f78d9b28d8aa49906a1
Abutalib (Barish) Namazov <barish2003@gmail.com>
2026-01-30 17:22:12 -0500

    Add og-image and fix typos

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
 export const SITE_TITLE = "kodlar.az";
 export const SITE_DESCRIPTION =
-  "İnformatika və proqramlaşdırma haqqında Azərbaycan dilində məqalələr, söhbətlər və ictimai resurslar";
+	"İnformatika, proqramlaşdırma və kompüter elmləri haqqında Azərbaycan dilində məqalələr, söhbətlər və ictimai resurslar.";
 export const GITHUB_URL = "https://github.com/BarishNamazov/kodlar.az";
 export const GITHUB_DISCUSSIONS_URL =
-  "https://github.com/BarishNamazov/kodlar.az/discussions";
+	"https://github.com/BarishNamazov/kodlar.az/discussions";
 export const CONTRIBUTING_URL =
-  "https://github.com/BarishNamazov/kodlar.az/blob/main/docs/CONTRIBUTING.md";
+	"https://github.com/BarishNamazov/kodlar.az/blob/main/docs/CONTRIBUTING.md";
 export const CONTACT_URL =
-  "https://github.com/BarishNamazov/kodlar.az/blob/main/docs/SUPPORT.md";
+	"https://github.com/BarishNamazov/kodlar.az/blob/main/docs/SUPPORT.md";
 export const README_URL =
-  "https://github.com/BarishNamazov/kodlar.az/blob/main/docs/README.md";
+	"https://github.com/BarishNamazov/kodlar.az/blob/main/docs/README.md";
 export const CODE_OF_CONDUCT_URL =
-  "https://github.com/BarishNamazov/kodlar.az/blob/main/docs/CODE_OF_CONDUCT.md";
+	"https://github.com/BarishNamazov/kodlar.az/blob/main/docs/CODE_OF_CONDUCT.md";
 export const YOUTUBE_URL = "https://youtube.com/@kodlaraz";
